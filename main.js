a2cd57a1934cafa2e67c491c86089e7173ada046
Dheysson S <78886796+dheysson@users.noreply.github.com>
2021-10-15 00:26:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let iframe;
 let tog;
 let hRange;
 let map = {}
+let image = '';
+let lightMode = localStorage.getItem('LightMode');
 
 document.addEventListener('keydown', (event) => {
     map[event.keyCode] = event.type == 'keydown'
@@ -23,7 +25,13 @@ function enableEditMode() {
     richTextField.document.body.style.fontFamily = 'Arial';
     richTextField.document.body.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae lacinia odio. Etiam non sollicitudin risus. Cras maximus, leo eget dictum porta, turpis sem convallis erat, ac viverra arcu nisl sit amet nibh. Nam posuere finibus enim et interdum. Mauris malesuada aliquet nulla a ultricies. Suspendisse non justo id risus luctus gravida. Nam a fermentum nisl, nec porttitor lorem.'
 
-    Dark();   
+    if(lightMode == 'dark'){
+        Dark();
+    } else if(lightMode == 'light'){
+        Light();
+    } else {
+        Dark();
+    }
 }
 function getElements(){
     toggle = document.getElementById("toggle-mode");
@@ -44,6 +52,8 @@ function toggleMode() {
     }
 }
 function Dark(){
+    localStorage.setItem('LightMode','dark');
+
     tog.className = 'fas fa-toggle-on'
     event.target.className = "fas fa-toggle-on";
     richTextField.document.body.style.color = '#fff';
@@ -57,6 +67,8 @@ function Dark(){
     }
 }
 function Light(){
+    localStorage.setItem('LightMode','light');
+
     event.target.className = "fas fa-toggle-off";
     richTextField.document.body.style.color = '#000';
     richTextField.document.body.classList.toggle('dark',false);
